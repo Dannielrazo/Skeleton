c98dfb8f813186c26367ce9197037d560821f31b
Daniel Simon <Daniel.razosc@gmail.com>
2022-02-06 02:07:16 -0600

    Editando cards

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+let skeleton = document.querySelectorAll('.skeleton')
+let option = document.querySelectorAll('.option')
+
+
+setTimeout(function(){
+    option.forEach(function(item){
+        item.classList.remove('loading')
+    })
+
+    skeleton.forEach(function(item){
+        item.classList.add('loading')
+    })
+    }, 3000);
